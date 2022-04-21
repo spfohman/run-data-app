1f02936555a9c13ea0fa0a97af34ca75da08c298
+ <+>
2022-04-21 15:15:44 -0700

    add namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # namespace :api do
+   namespace :api do
 
   resources :runs, only: [:index, :show, :create, :update, :destroy]
   resources :users
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-# end
+ end
 # # Routing logic: fallback requests for React Router.
 # # Leave this here to help deploy your app later!
 
-# get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+ get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
